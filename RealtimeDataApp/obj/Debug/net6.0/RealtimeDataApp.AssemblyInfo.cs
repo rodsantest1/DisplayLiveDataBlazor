8c94694edf84db01815ee4334bbf092b8fd7c0ce
Rodney Santiago <rodney@rcsan96.onmicrosoft.com>
2024-09-02 22:21:06 -0400

    finishing fork

--- RealtimeDataApp/obj/Debug/net6.0/RealtimeDataApp.AssemblyInfo.cs
+++ RealtimeDataApp/obj/Debug/net6.0/RealtimeDataApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealtimeDataApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67f92c6b6865219e5146dce2fb58ea1530b58b58")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealtimeDataApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealtimeDataApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
